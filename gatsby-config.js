a200c6b45cd290b463370e207f842391d6031b6f
Eric N. Garcia <engarcia618@gmail.com>
2020-05-26 16:47:48 -0400

    style: add eslint and fix all errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,39 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Default Starter',
+    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@gatsbyjs',
   },
   plugins: [
     {
-      resolve: `gatsby-theme-notes`,
+      resolve: 'gatsby-theme-notes',
       options: {
         // basePath defaults to `/`
-        basePath: `/notes`,
+        basePath: '/notes',
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `Eric N. Garcia`,
-        short_name: `ENG618`,
-        start_url: `/`,
-        background_color: `#303F9F`,
-        theme_color: `#303F9F`,
-        display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        name: 'Eric N. Garcia',
+        short_name: 'ENG618',
+        start_url: '/',
+        background_color: '#303F9F',
+        theme_color: '#303F9F',
+        display: 'standalone',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
   ],
-}
+};
