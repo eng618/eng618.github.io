2697a6888d599f78f5848dc6985f987809049758
Eric N. Garcia <eng618@garciaericn.com>
2021-11-17 12:05:08 -0500

    feat: add contact page

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from 'gatsby-theme-carbon/src/components/Layout';
+import { container } from 'gatsby-theme-carbon/src/components/FourOhFour/FourOhFour.module.scss';
+
+const ContactMe = () => (
+  <Layout>
+    <div className={container}>
+      <iframe
+        title="Contact "
+        src="https://docs.google.com/forms/d/e/1FAIpQLSezjyM8b4tieDTgFccofUAzogvOfbDZUvN52ftg51IUS8CHQw/viewform?embedded=true"
+        width="100%"
+        height="1000px"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+      >
+        <div className="spinner" />
+      </iframe>
+    </div>
+  </Layout>
+);
+
+export default ContactMe;
