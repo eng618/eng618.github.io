001b3c91e4de3e99c369d26ecb14bf07273cab70
Eric N. Garcia <eng618@garciaericn.com>
2022-01-31 09:58:36 -0500

    styles: update some styles to newer react format

--- src/gatsby-theme-carbon/components/Footer/index.jsx
+++ src/gatsby-theme-carbon/components/Footer/index.jsx
@@ -2,25 +2,27 @@ import React from 'react';
 import Footer from 'gatsby-theme-carbon/src/components/Footer';
 import PropTypes from 'prop-types';
 
-const Content = ({ buildTime }) => (
-  <>
-    <p>Personal site for work related notes and documentation.</p>
-    <p>
-      This site was last updated on:
-      <br />
-      {buildTime}
-    </p>
-    <p>
-      <a href="https://github.com/eng618/eng618.github.io/actions/workflows/build-and-deploy.yml">
-        <img
-          src="https://github.com/eng618/eng618.github.io/actions/workflows/build-and-deploy.yml/badge.svg"
-          alt="Build Status"
-        />
-      </a>
-    </p>
-    <p>© 2021 Garcia Enterprise</p>
-  </>
-);
+function Content({ buildTime }) {
+  return (
+    <>
+      <p>Personal site for work related notes and documentation.</p>
+      <p>
+        This site was last updated on:
+        <br />
+        {buildTime}
+      </p>
+      <p>
+        <a href="https://github.com/eng618/eng618.github.io/actions/workflows/build-and-deploy.yml">
+          <img
+            src="https://github.com/eng618/eng618.github.io/actions/workflows/build-and-deploy.yml/badge.svg"
+            alt="Build Status"
+          />
+        </a>
+      </p>
+      <p>© 2021 Garcia Enterprise</p>
+    </>
+  );
+}
 
 const links = {
   firstCol: [
@@ -36,7 +38,9 @@ const links = {
   secondCol: [],
 };
 
-const CustomFooter = () => <Footer links={links} Content={Content} />;
+function CustomFooter() {
+  return <Footer links={links} Content={Content} />;
+}
 
 Content.propTypes = {
   buildTime: PropTypes.string.isRequired,
--- src/gatsby-theme-carbon/components/Header/index.jsx
+++ src/gatsby-theme-carbon/components/Header/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from 'gatsby-theme-carbon/src/components/Header';
 
-const CustomHeader = (props) => (
-  <Header {...props}>
-    Eric N.&nbsp;<span>Garcia</span>
-  </Header>
-);
+function CustomHeader(props) {
+  return (
+    <Header {...props}>
+      Eric N.&nbsp;<span>Garcia</span>
+    </Header>
+  );
+}
 
 export default CustomHeader;
